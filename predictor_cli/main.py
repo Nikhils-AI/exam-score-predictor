014b9e858ca02b3bc6b2ea63af65b94b482c492e
Nikhil Singh <nikhils1220@gmail.com>
2025-07-20 20:59:27 -0700

    script is nearly complete

--- predictor_cli/main.py
+++ predictor_cli/main.py
@@ -1,11 +1,42 @@
 from joblib import load
 import numpy as np
 
-pipe = load('pipe.joblib')
+# load in estimator from notebook
+model = load('pipe.joblib')
 
-sample = np.array([23, 1, 0.0, 1.2, 1.1, 0, 85.0,
-                   8.0, 1, 6, 2, 1, 8, 1]).reshape(1, -1)
+# begin python script
+print("\t\tEXAM SCORE PREDICTOR\t\t")
+print("+----------------------------------+")
+print("Predict your exam score by answering \n"
+      "the questions below!\n")
+
+# query age
+age = int(input("What is your age? ").strip())
+
+# query gender
+genders = {'male', 'female', 'other'}
+gender_conversion = {"male": 0, "female": 1, "other": 2}
+
+gender = input("What is your gender? [male/female/other]: ").strip().lower()
+while True:
+    if gender in genders:
+        break
+    gender = input(f"Invalid choice: {gender}. Please enter one of "
+                   f"[male/female/other]: ")
+
+gender = gender_conversion[gender]
+
+# query study hours per day
+study_hours = int(input("On average, how many hours did you study per day? "))
+
+# query social media hours per day
+social_media_hours = int(input("On average, how many hours did you spend on "
+                               "social media per day? "))
+
+# query netflix hours per day
+netflix_hours = int(input("On average, how many hours did you spend on "
+                          "netflix per day? "))
 
 
-print(pipe.predict(sample))
+
 
