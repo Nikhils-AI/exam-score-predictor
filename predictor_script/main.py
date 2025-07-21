40049349019513f6d82fd649ecb755150c5d22b4
Nikhil Singh <nikhils1220@gmail.com>
2025-07-21 14:16:20 -0700

    added many inputs

--- predictor_script/main.py
+++ predictor_script/main.py
@@ -1,41 +1,97 @@
 from joblib import load
 import numpy as np
 
-# load in estimator from notebook
+# load the model from notebook
 model = load('pipe.joblib')
 
-# begin python script
+# begin the python script
 print("\t\tEXAM SCORE PREDICTOR\t\t")
 print("+----------------------------------+")
 print("Predict your exam score by answering \n"
       "the questions below!\n")
 
-# query age
-age = int(input("What is your age? ").strip())
+# query the user's age
+age_range = tuple(range(0, 111))
+age = int(input("What is your age? [0-110]: "))
 
-# query genderS
-genders = {'male', 'female', 'other'}
+while True:
+    if age in age_range:
+        break
+    age = int(input(f"Out of range: {age}\nWhat is your age? [0-110]: "))
+
+# query the user's gender
+gender_choices = {'male', 'female', 'other'}
 gender_conversion = {"male": 0, "female": 1, "other": 2}
 
 gender = input("What is your gender? [male/female/other]: ").strip().lower()
 while True:
-    if gender in genders:
+    if gender in gender_choices:
         break
     gender = input(f"Invalid choice: {gender}. Please enter one of "
-                   f"[male/female/other]: ")
+                   f"[male/female/other]: ").strip().lower()
 
 gender = gender_conversion[gender]
 
-# query study hours per day
-study_hours = int(input("On average, how many hours did you study per day? "))
+# query the user's study hours per day
+hours_range = tuple(range(0, 25))
+study_hours = int(input("On average, how many hours did you study per day? "
+                        "[0-24]: "))
 
-# query social media hours per day
+while True:
+    if study_hours in hours_range:
+        break
+    study_hours = int(input(f"Out of range: {study_hours}.\n"
+                            f"On average, how many hours did you study per day?"
+                            f" [0-24]: "))
+
+# query the user's social media hours per day
 social_media_hours = int(input("On average, how many hours did you spend on "
                                "social media per day? "))
 
-# query netflix hours per day
+# query the user's netflix hours per day
 netflix_hours = int(input("On average, how many hours did you spend on "
                           "netflix per day? "))
+
+# query the user's employment status
+emp_status_choices = {'yes', 'no'}
+emp_status_conversion = {'no': 0, 'yes': 1}
+
+emp_status = input("Do you have a part-time job? [yes/no]: ").strip().lower()
+while True:
+    if emp_status in emp_status_choices:
+        break
+    job_status = input(f"Invalid choice: {emp_status}. Please enter one of"
+                       f"[yes/no]: ").strip().lower()
+
+job_status = emp_status_conversion[emp_status]
+
+# query the user's attendance percentage
+attendance_pct = int(input("What is your attendance percentage? "
+                           "[enter decimal]: "))
+
+
+# query the user's sleep hours per day
+sleep_hours = int(input("On average, how many hours do you sleep per night? "))
+
+# query the user's diet quality
+diet_quality_choices = {'poor', 'fair', 'good'}
+diet_quality_conversion = {'poor': 0, 'fair': 1, 'good': 2}
+
+diet_quality = input("What is your diet quality? [poor/fair/good]:"
+                     " ").strip().lower()
+
+while True:
+    if diet_quality in diet_quality_choices:
+        break
+    diet_quality = input("Invalid choice. Please enter one of [poor/fair/good]"
+                         ": ").strip().lower()
+
+diet_quality = diet_quality_conversion[diet_quality]
+
+# query the user's exercise_frequency
+exercise_frequency = int(input("How many days per week do you exercise?"))
+
+
 
 
 
